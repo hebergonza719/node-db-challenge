c278b1141a06ee4682355808198192e775f05482
Heber <hebergonza@gmail.com>
2020-04-18 16:28:57 -0400

    finished mvp, answered questions

--- projects/projects-model.js
+++ projects/projects-model.js
@@ -1,10 +1,40 @@
 const db = require('../data/db-config.js');
 
+function addResource(resourceData) {
+  return db('resources')
+    .insert(resourceData);
+}
 
+function getResources() {
+  return db('resources');
+}
 
+function addProject(projectData) {
+  return db('projects')
+    .insert(projectData);
+}
 
+function getProjects() {
+  return db('projects');
+}
+
+function addTask(taskData) {
+  return db('tasks')
+    .insert(taskData);
+}
+
+function getTasks() {
+  return db('projects as p')
+    .join('tasks as t', 'p.id', 't.project_id')
+    .select('t.description as Task Description', 'p.name as Project Name', 'p.description as Project Description')
+}
 
 
 module.exports = {
-
+  addResource,
+  getResources,
+  addProject,
+  getProjects,
+  addTask,
+  getTasks
 }
--- projects/projects-router.js
+++ projects/projects-router.js
@@ -4,6 +4,71 @@ const Projects = require('./projects-model.js');
 
 const router = express.Router();
 
+router.post('/resources', (req, res) => {
+  resourceData = req.body;
 
+  Projects.addResource(resourceData)
+    .then(resource => {
+      res.status(201).json(resource);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Couldn't access database" });
+    });
+});
+
+router.get('/resources', (req, res) => {
+  Projects.getResources()
+    .then(resources => {
+      res.json(resources);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Couldn't access database" });
+    });
+});
+
+router.post('/', (req, res) => {
+  projectData = req.body;
+
+  Projects.addProject(projectData)
+    .then(project => {
+      res.status(201).json(project);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Couldn't access database" });
+    });
+});
+
+router.get('/', (req, res) => {
+  Projects.getProjects()
+    .then(projects => {
+      res.json(projects);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Couldn't access database" });
+    });
+});
+
+router.post('/tasks', (req, res) => {
+  taskData = req.body;
+
+  Projects.addTask(taskData)
+    .then(task => {
+      res.status(201).json(task);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Couldn't access database" });
+    });
+});
+
+router.get('/tasks', (req, res) => {
+  
+  Projects.getTasks()
+    .then(tasks => {
+      res.json(tasks);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Couldn't access database" })
+    });
+})
 
 module.exports = router;
